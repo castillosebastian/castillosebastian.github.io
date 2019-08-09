33e3f4f8d1e034ad78b96c7b8128952716f92c78
sebacastillo <castilloclaudiosebastian@gmail.com>
2019-08-08 22:04:23 -0300

    actualizo

---
+++ misc/opciones.R
@@ -0,0 +1,12 @@
+# compro SUPVC61.0AG a $8 cotización de hoy SUPV 67,35
+egreso_compra = 8 * 100
+egreso_ejercicio = 61*100
+egreso_compra + egreso_ejercicio
+#valor intrínseco Precio Accion - Precio Ejercicio (strike)
+valor_intrinseco = 67.35 - 61
+# Porqué la prima está mas cara que el valor intrínseco?
+
+# comprar C barato. ejemplo
+egreso_compra_ejemplo = 6.2 *100
+egreso_ejercicio = 61*100
+egreso_compra_ejemplo + egreso_ejercicio
