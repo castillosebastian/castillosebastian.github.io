94a2da472771dcdfcac713c0896edc934d52c6e0
sebacastillo <castilloclaudiosebastian@gmail.com>
2019-11-22 21:07:32 -0300

    art

---
+++ R/aRt.R
@@ -0,0 +1,4 @@
+# art code
+#install.packages(c("devtools", "mapproj", "tidyverse", "ggforce", "Rcpp"))
+#devtools::install_github("marcusvolz/mathart")
+# devtools::install_github("marcusvolz/ggart")
